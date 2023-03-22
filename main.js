024e1b3585a1b2ba61f266d85005d3096bbbb49b
ishveer1026 <107894915+ishveer1026@users.noreply.github.com>
2023-03-22 16:57:44 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function g(){
+    window.location="guitar.html";
+}
+function pi(){
+    window.location="Piano.html";
+}
+function h(){
+    window.location="Harmonium.html";
+}
+function pr(){
+    window.location="printer.html"
+}
+function t(){
+    window.location="TissueBox.html";
+}
